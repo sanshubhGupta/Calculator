9a03a9e601f7769c9862b76f3a81a4ee7b3971e9
Sanshubh Gupta <sanshubh08gupta@gmail.com>
2024-02-04 20:22:07 +0530

    Add decimal support for buttons

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let flag = 0;
 //flag = 0 represents that operator not entered, i.e, only one number on display
 let checkIfNum2Entered = 0;
 //checkIfNum2Entered = 0 represents that num2 has not been started entering, operator may have been entered
+let decimal = 0;
 let ans = 0;
 let operator = '';
 let displayText = "0";
@@ -73,15 +74,37 @@ function operate(num1, num2, operator){
 numbers.forEach(number=>{
     number.addEventListener('click', function(){
         let num = number.textContent;
-        if(flag == 0){
-            num1 = (num1*10) + parseFloat(num);
+        if(num == '.' && decimal == 0 || num != '.'){
+            displayText = (displayText!=0) ? displayText + num : num;
+            display.innerHTML = displayText;
+        }
+        if(num == '.'){
+            if(decimal == 0){
+                decimal++;
+            }
+        }
+        else if(decimal > 0){
+            if(flag == 0){
+                num = num/(10**decimal);
+                decimal++;
+                num1 = parseFloat(num1) + parseFloat(num);
+            }
+            else{
+                num = num/(10**decimal);
+                decimal++;
+                num2 = parseFloat(num2) + parseFloat(num);
+                checkIfNum2Entered = 1;
+            }
         }
         else{
-            num2 = (num2*10) + parseFloat(num);
-            checkIfNum2Entered = 1;
+            if(flag == 0){
+                num1 = (num1*10) + parseFloat(num);
+            }
+            else{
+                num2 = (num2*10) + parseFloat(num);
+                checkIfNum2Entered = 1;
+            }
         }
-        displayText = (displayText!=0) ? displayText + num : num;
-        display.innerHTML = displayText;
     });
 });
 
@@ -92,6 +115,7 @@ operators.forEach(oper=>{
             displayText += ` ${operator} `;
             display.innerHTML = displayText;
             flag = 1;
+            decimal = 0;
         }
         else{
             num1 = operate(num1, num2, operator);
@@ -106,12 +130,14 @@ operators.forEach(oper=>{
 
 equals.addEventListener('click', function(){
     flag = 0;
+    console.log(num1);
+    console.log(num2);
     if(operator!=''){
-        console.log(num1);
         num1 = operate(num1, num2, operator);
         console.log(num1);
     }
-    num1 = Number.isInteger(num1) ? num1 : num1.toFixed(3);
+    num1 = Number.isInteger(num1) ? num1 : num1.toFixed(4);
+    decimal = Number.isInteger(num1) ? 0 : 4;
     displayText = `${num1}`;
     display.innerHTML = displayText;
     checkIfNum2Entered = 0;
@@ -127,41 +153,86 @@ clearAll.addEventListener('click', function(){
     operator = '';
     displayText = "0";
     display.innerHTML = displayText;
+    decimal = 0;
 });
 
 del.addEventListener('click', function(){
     if(flag == 0){
-        num1 = (num1 - num1 % 10) / 10;
-        displayText = displayText.substring(0, displayText.length - 1);
-        display.innerHTML = displayText;
-        if(num1 == 0){
-            num1 = 0;
-            num2 = 0;
-            flag = 0;
-            checkIfNum2Entered = 0;
-            ans = 0;
-            operator = '';
-            displayText = "0";
+        if(decimal == 0){
+            num1 = (num1 - num1 % 10) / 10;
+            displayText = displayText.substring(0, displayText.length - 1);
             display.innerHTML = displayText;
+            if(num1 == 0){
+                num1 = 0;
+                num2 = 0;
+                flag = 0;
+                checkIfNum2Entered = 0;
+                ans = 0;
+                operator = '';
+                displayText = "0";
+                display.innerHTML = displayText;
+            }
         }
+        //for deletion of numbers after decimal
+        else if(displayText[displayText.length - 1] != '.'){
+            let dig = parseInt(displayText[displayText.length - 1]);
+            displayText = displayText.substring(0, displayText.length - 1);
+            display.innerHTML = displayText;
+            num1 -= dig/(10 ** (decimal - 1));
+            num1 = num1.toFixed(decimal - 1);
+            decimal--;
+        }
+        //for deletion of decimal point
+        else{
+            displayText = displayText.substring(0, displayText.length - 1);
+            display.innerHTML = displayText;
+            num1 = parseInt(num1);
+            decimal--;
+        }
+        console.log(num1);
+        console.log(decimal);
     }
     else{
         let check1 = displayText[displayText.length - 2];//to check if the last char is operator
         let check2 = displayText[displayText.length - 3];//to check if 2nd last operator is operator, or only one dig num left in num2
-        if(!isNaN(check1)){
-            num2 = (num2 - num2 % 10) / 10;
+        if(decimal ==  0){
+            if(!isNaN(check1)){
+                num2 = (num2 - num2 % 10) / 10;
+                displayText = displayText.substring(0, displayText.length - 1);
+                display.innerHTML = displayText;
+                if(isNaN(check2)){
+                    checkIfNum2Entered = 0;
+                }
+            }
+            else{
+                flag = 0;
+                operator = '';
+                displayText = displayText.substring(0, displayText.length - 3);
+                display.innerHTML = displayText;
+            }
+        }
+        //for deletion of numbers after decimal
+        else if(displayText[displayText.length - 1] != '.'){
+            let dig = parseInt(displayText[displayText.length - 1]);
             displayText = displayText.substring(0, displayText.length - 1);
             display.innerHTML = displayText;
+            num2 -= dig/(10 ** (decimal - 1));
+            num2 = num2.toFixed(decimal - 1);
+            decimal--;
+        }
+        //for deletion of decimal point
+        else{
+            console.log('here');
+            displayText = displayText.substring(0, displayText.length - 1);
+            display.innerHTML = displayText;
+            num2 = parseInt(num1);
+            decimal--;
             if(isNaN(check2)){
                 checkIfNum2Entered = 0;
             }
         }
-        else{
-            flag = 0;
-            operator = '';
-            displayText = displayText.substring(0, displayText.length - 3);
-            display.innerHTML = displayText;
-        }
+        console.log(num2);
+        console.log(decimal);
     }
 });
 
